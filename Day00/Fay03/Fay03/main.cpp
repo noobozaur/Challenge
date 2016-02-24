27f842d7decaf2de906cfe2543667f653c317a2d
Vladimir <vovo4ka08@mail.ru>
2016-02-25 00:24:59 +0400

    string test

---
+++ Day00/Fay03/Fay03/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+
+	char vovaDegay[] = {'V', 'o', 'v', 'a', 'D', 'e', 'g', 'a', 'y', '\0'};
+
+	for (int i = 0; i<9; i++)
+		cout<<vovaDegay[i];
+
+	cout<<"\n\n";
+
+	return 0;
+
+}
+
